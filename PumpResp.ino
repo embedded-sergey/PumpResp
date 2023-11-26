f4b11e6d5d85fe90e4f1ac8b9bc4bb8ecc593af9
embedded-sergey <embedded.sergey@gmail.com>
2023-11-27 00:43:58 +0200

    Fix active high and low relay issues

--- PumpResp.ino
+++ PumpResp.ino
@@ -1,12 +1,17 @@
-// Version 1.0
+// Version 1.1
 
 ////////////////////
 // USER INTERFACE //
 ////////////////////
 
 // Set the length of measurement and flush phases (in seconds)
-int FLUSH = 4;       // LED is on, flushing pump is on
-int MEASUREMENT = 4; // LED is blinking, flushing pump is off
+int FLUSH = 5;        // LED is on, flushing pump is on
+int MEASUREMENT = 15; // LED is blinking, flushing pump is off
+
+// By default, the relay is active low. 
+// Thus, if your relay is active high, reverse the variable values:
+const byte RELAY_ON = LOW;
+const byte RELAY_OFF = HIGH;
 
 /////////////////////////
 // IMPLEMENTATION CODE //
@@ -27,8 +32,8 @@ void setup()
 
 void loop()
 {
-   digitalWrite(RELAY, LOW); // set pin 3 OFF to turn off the relay,
-                        // so pumps start working again.
+   digitalWrite(RELAY, RELAY_OFF); // set pin 3 OFF to turn off the relay,
+                                  // so pumps start working again.
 
    for(int counter = 0; counter < FLUSH; counter++)
       {
@@ -37,8 +42,8 @@ void loop()
          delay(1000);
       }
 
-   digitalWrite(RELAY, HIGH); // set pin 3 ON to activate the relay,
-                         // so pumps stop working
+   digitalWrite(RELAY, RELAY_ON); // set pin 3 ON to activate the relay,
+                              // so pumps stop working
 
    for(int counter = 0; counter < MEASUREMENT; counter++)
       {
